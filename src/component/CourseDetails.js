598b43421648888a1e901ae26a75a6d202591267
Omkar Dafale <84869739+OmkarDafale@users.noreply.github.com>
2023-03-28 17:29:32 +0530

    New changes

--- src/component/CourseDetails.js
+++ src/component/CourseDetails.js
@@ -1,24 +1,39 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-const CourseDetails = () => {
-  return (
-    <div className="container my-2">
-        <h1>Course Details</h1>
-        <div className="card">
-          <div className="card-header">JavaScript</div>
-          <div className="card-body">
-            <h5 className="card-title">Special title treatment</h5>
-            <p className="card-text">
-              With supporting text below as a natural lead-in to additional
-              content.
-            </p>
-            <Link to="/" className="btn btn-primary">
-              Enroll now
-            </Link>
-          </div>
-        </div>
-      </div>
-  )
-}
+import React, { useEffect, useState } from "react";
+import CourseItem from "./CourseItem";
 
-export default CourseDetails
+const CourseDetails = () => {
+  const [courses, setCourses] = useState([])
+  const getCourse = async () => {
+    const url = "https://hub.dummyapis.com/vj/mmqG4S6";
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const json = await response.json();
+    setCourses(json);
+  };
+
+  useEffect(() => {
+    getCourse()
+  }, [])
+
+  useEffect(() => {
+    console.log(courses)
+  }, [courses])
+  
+  
+  return (
+    <div className="container">
+      <h1>Course Details</h1>
+      <ul>
+          {courses.map((course) => (
+            <CourseItem key={course.id} course={course}/>
+          ))}
+        </ul>
+    </div>
+  );
+};
+
+export default CourseDetails;
