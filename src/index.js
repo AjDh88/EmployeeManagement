5cce386539575c4fe52480a98dadaec311d149fd
Ajay Dhabade <101987588+AjDh88@users.noreply.github.com>
2023-03-28 18:16:14 +0530

    keycloak changes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Keycloak from 'keycloak-js';
 
+console.log("Start");
+let keycloak = new Keycloak('./resources/keycloak.json');
+console.log("Keycloak init");
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App />    
-);
+keycloak.init({ onLoad: 'login-required' }).then(() => {
+    console.log("Inside console log");
+    ReactDOM.render(
+        console.log("App initiated"),
+        <App keycloak={keycloak} />,
+        document.getElementById('root')
+    );
+});
